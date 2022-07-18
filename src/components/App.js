d1bf50365d5c7ed7ed710bd8929146d99ff50b46
Nicholas <nicholasjoven@gmail.com>
2022-07-18 13:23:39 -0400

    commit tree 89cd323448d62ed9df2a22cec03d8958f1cc6a5b

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import About from "./About";
+import NavBar from "./NavBar";
+import Home from "./Home";
 
 function App() {
   return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
+      { NavBar() }
+      { Home() }
+      { About() }
     </div>
   );
 }
